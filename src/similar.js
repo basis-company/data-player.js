fd622483b83d57bf4904150396e80f6f3b017e1b
Ilya Anisimov <litstat@gmail.com>
2024-09-12 02:37:03 +0300

    test similar

--- src/similar.js
+++ src/similar.js
@@ -1,4 +1,5 @@
 import { nativeIsArray } from 'underscore/modules/_setup.js';
+import noop from 'underscore/modules/noop.js';
 
 import { applyOwnIf } from 'helpers/apply';
 import { empty } from 'helpers/empty';
@@ -112,3 +113,140 @@ function params(candidate, loader) {
   return empty(params) ||
     applyOwnIf(params, loader.params);
 }
+
+function test() {
+  var c = {};
+  var l = {};
+
+  range$.forEach(o => {
+    o.fn(c, l);
+    assert[o.res](range(c, l), o.msg);
+  });
+
+  param$.forEach(o => {
+    o.fn(c, l);
+
+    var p    = {};
+    var res  = params(c, l, p);
+    var diff = JSON.stringify(p.params || {});
+
+    if (res === o.res && diff === (o.diff || '{}')) {
+      return;
+    }
+
+    raise(o.msg + ', params = ' + diff);
+  });
+}
+
+const range$ = [
+  {
+    res: 'truthy',
+    msg: 'range: both empty',
+    fn: noop,
+  }, {
+    res: 'truthy',
+    msg: 'range: empty candidate',
+    fn(c, l) { l.range = [ 20201101, 20201130 ]; },
+  }, {
+    res: 'truthy',
+    msg: 'range: equals',
+    fn(c)    { c.range = [ 20201101, 20201130 ]; },
+  }, {
+    res: 'truthy',
+    msg: 'range: grab range',
+    fn(c)    { c.range = [ 20201030, 20201201 ]; },
+  }, {
+    res: 'truthy',
+    msg: 'range: expand left',
+    fn(c)    { c.range = [ 20201015, 20201130 ]; },
+  }, {
+    res: 'truthy',
+    msg: 'range: expand right',
+    fn(c)    { c.range = [ 20201101, 20201215 ]; },
+  }, {
+    res: 'falsey',
+    msg: 'range: shift left',
+    fn(c)    { c.range = [ 20201015, 20201115 ]; },
+  }, {
+    res: 'falsey',
+    msg: 'range: shift right',
+    fn(c)    { c.range = [ 20201115, 20201215 ]; },
+  }, {
+    res: 'falsey',
+    msg: 'range: entire left',
+    fn(c)    { c.range = [ 20201015, 20201020 ]; },
+  }, {
+    res: 'falsey',
+    msg: 'range: entire right',
+    fn(c)    { c.range = [ 20201205, 20201215 ]; },
+  },
+];
+
+const param$ = [
+  {
+    res: true,
+    msg: 'params: both empty',
+    fn(c, l) {
+      c.params = {};
+      l.params = {};
+    },
+  }, {
+    res: true,
+    msg: 'params: empty candidate',
+    fn(c, l) {
+      c.params = {};
+      l.params = { month: 1 };
+    },
+  }, {
+    res: true,
+    msg: 'params: equals',
+    fn(c, l) {
+      c.params = { month: 1, day: [ 1, 2 ] };
+      l.params = { month: 1, day: [ 1, 2 ] };
+    },
+  }, {
+    res: true,
+    msg: 'params: grab range',
+    fn(c, l) {
+      c.params = { month: 1, day: [ 1, 2 ] };
+      l.params = { month: 1, day: 1, sector: [ 5, 6 ] };
+    },
+  }, {
+    res: false,
+    msg: 'params: request wide range',
+    diff: '{"day":[2],"month":1}',
+    fn(c, l) {
+      c.params = { month: 1, day: 1 };
+      l.params = { month: 1, day: [ 1, 2 ] };
+    },
+  }, {
+    res: false,
+    msg: 'params: request wide param',
+    fn(c, l) {
+      c.params = { month: 1, day: [ 1, 2 ], sector: 5 };
+      l.params = { month: 1, day: 1 };
+    },
+  }, {
+    res: false,
+    msg: 'params: non-crossed outside range',
+    fn(c, l) {
+      c.params = { month: 1, day: [ 1, 2 ] };
+      l.params = { month: 1, day: [ 3, 4 ] };
+    },
+  }, {
+    res: false,
+    msg: 'params: allow single outside range',
+    fn(c, l) {
+      c.params = { month: 1, day: [ 1, 2 ], sector: [ 5, 7 ] };
+      l.params = { month: 1, day: [ 2, 3 ], sector: [ 5, 6 ] };
+    },
+  }, {
+    res: false,
+    msg: 'params: subrange',
+    diff: '{"day":[3],"month":1,"sector":[5]}',
+    fn(c, l) {
+      c.params = { month: 1, day: [ 1, 2 ], sector: [ 5, 7 ] };
+      l.params = { month: 1, day: [ 2, 3 ], sector: [ 5 ] };
+    },
+  },
+];
